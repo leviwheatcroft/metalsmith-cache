6d9cb91ffcca4cbbcd15f576a2023d342844d467
Levi Wheatcroft <levi@wheatcrofts.com.au>
2017-05-22 06:47:54 +0800

    switched to class structure

--- dist/index.js
+++ dist/index.js
@@ -3,10 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.files = exports.loki = undefined;
-exports.init = init;
-exports.save = save;
-exports.getStore = getStore;
+exports.save = exports.init = exports.ValueCache = exports.FileCache = exports.loki = undefined;
 
 var _lokijs = require('lokijs');
 
@@ -48,44 +45,52 @@ function save(cb) {
   return defer.promise();
 }
 
-const files = exports.files = loki.getCollection('files') || loki.addCollection('files', { unique: 'path' });
-files.store = (path, file) => {
-  const doc = { path, file };
-  const existing = files.findOne({ path });
-  if (existing) return files.update(existing, doc);
-  return files.insert(doc);
-};
-files.retrieve = path => {
-  const result = files.findOne({ path });
-  if (result) return result.file;
-};
-files.all = () => {
-  const results = {};
-  files.data.forEach(doc => {
-    results[doc.path] = doc.file;
-  });
-  return results;
-};
-files.match = mask => {
-  files.where(doc => {
-    return (0, _multimatch2.default)(mask, [doc.path]);
-  });
-};
+class FileCache {
+  constructor(name) {
+    this.collection = loki.getCollection(name) || loki.addCollection(name, { unique: 'path' });
+  }
+  store(path, file) {
+    const doc = { path, file };
+    const existing = this.collection.findOne({ path });
+    if (existing) return this.collection.update(existing, doc);
+    return this.collection.insert(doc);
+  }
+  retrieve(path) {
+    const result = this.collection.findOne({ path });
+    if (result) return result.file;
+  }
+  all() {
+    const results = {};
+    this.collection.data.forEach(doc => {
+      results[doc.path] = doc.file;
+    });
+    return results;
+  }
+  match(mask) {
+    this.collection.where(doc => (0, _multimatch2.default)(mask, [doc.path]));
+  }
+}
 
-function getStore(name) {
-  const store = loki.getCollection(name) || loki.addCollection(name, { unique: 'key' });
-  store.store = (key, value) => {
-    const existing = store.findOne({ key });
+class ValueCache {
+  constructor(name) {
+    this.collection = loki.getCollection(name) || loki.addCollection(name, { unique: 'key' });
+  }
+  store(key, value) {
+    const existing = this.collection.findOne({ key });
     if (existing) {
       existing.value = value;
-      store.update(existing);
+      this.collection.update(existing);
     } else {
-      store.insert({ key, value });
+      this.collection.insert({ key, value });
     }
-  };
-  store.retrieve = key => {
-    const result = store.findOne({ key });
+  }
+  retrieve(key) {
+    const result = this.collection.findOne({ key });
     if (result) return result.value;
-  };
-  return store;
+  }
 }
+
+exports.FileCache = FileCache;
+exports.ValueCache = ValueCache;
+exports.init = init;
+exports.save = save;
